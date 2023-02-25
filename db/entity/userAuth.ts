09cbba7057f8a6bb3a257ef5fc7748e2cc0cd905
luosicheng379 <luosicheng379@hellobike.com>
2023-02-26 01:07:08 +0800

    feat: 数据库调通

---
+++ db/entity/userAuth.ts
@@ -0,0 +1,30 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from './user';
+
+@Entity({ name: 'user_auth' })
+export class UserAuth extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  readonly id!: number;
+
+  @ManyToOne(() => User, {
+    cascade: true,
+  })
+  @JoinColumn({ name: 'user_id' })
+  user!: User;
+
+  @Column()
+  identity_type!: string;
+
+  @Column()
+  identifier!: string;
+
+  @Column()
+  introduce!: string;
+}
--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -1,9 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { withIronSessionApiRoute } from 'iron-session/next';
 import { ironOptions } from 'config';
+import { prepareConnection } from 'db';
+import { User, UserAuth } from 'db/entity/index';
 
 async function login(req: NextApiRequest, res: NextApiResponse) {
   const { phone = '', verify = '' } = req.body;
+  const db = await prepareConnection();
+  console.log(db, 'db');
+  
+  const userRepo = db.getRepository(User);
+  console.log(userRepo, 'userRepo');
+  
+  const users = await userRepo.find()
+  console.log(users)
   res?.status(200)?.json({ phone, verify, code: 0 });
 }
 
